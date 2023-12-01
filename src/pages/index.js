676b1c7a7bb6b77ccd6476f57d252a227e633dcf
Furkan Pehlivan <furkan.pehlivan@merzigo.com>
2023-12-01 03:22:19 +0300

    simple bingo logic completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,86 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+import { v4 as uuid } from "uuid";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [numbers, setNumbers] = useState([]);
+  const [closedNumbers, setClosedNumbers] = useState({
+    firstRow: 0,
+    secondRow: 0,
+    thirdRow: 0,
+    numbers: [],
+  });
+  const [sack, setSack] = useState(
+    Array.from({ length: 90 }, (_, index) => index + 1)
+  );
+
+  const drawNumber = () => {
+    if (sack.length > 0) {
+      const randomIndex = Math.floor(Math.random() * sack.length);
+      const drawnNumber = sack[randomIndex];
+
+      const updateClosedNumbers = (index, row) => {
+        setClosedNumbers((current) => ({
+          ...current,
+          [row]: current[row] + 1,
+          numbers: [...current.numbers, drawnNumber],
+        }));
+      };
+
+      setSack((prevSack) => {
+        const updatedSack = prevSack.filter(
+          (_, index) => index !== randomIndex
+        );
+        if (numbers.includes(drawnNumber)) {
+          const index = numbers.indexOf(drawnNumber);
+          if (index < 9) updateClosedNumbers(index, "firstRow");
+          if (index > 8 && index < 18) updateClosedNumbers(index, "secondRow");
+          if (index > 17) updateClosedNumbers(index, "thirdRow");
+        }
+        return updatedSack;
+      });
+      console.log("drawn number: ", drawnNumber);
+      return drawnNumber;
+    } else {
+      console.log("No numbers left in the array.");
+      return null;
+    }
+  };
+
+  const createCard = () => {
+    const numbers = new Set();
+
+    const numbersArray = Array.from({ length: 27 }, (_, index) => {
+      if ((index + 1) % 2 === 0 || index === 8 || index === 26) {
+        let number;
+        do {
+          number = Math.floor(Math.random() * 89) + 1;
+        } while (numbers.has(number));
+
+        numbers.add(number);
+        return number;
+      } else {
+        return 0;
+      }
+    });
+    setNumbers(numbersArray);
+  };
+
+  useEffect(() => {
+    createCard();
+  }, []);
+
+  useEffect(() => {
+    if (closedNumbers.numbers.length === 15)
+      console.log(`TOMBALA! Remaining in the sack : ${sack.length}`);
+    else {
+      if (closedNumbers.firstRow === 5) console.log("First row completed");
+      if (closedNumbers.secondRow === 5) console.log("Second row completed");
+      if (closedNumbers.thirdRow === 5) console.log("Third row completed");
+    }
+  }, [closedNumbers]);
+
   return (
     <>
       <Head>
@@ -14,101 +89,28 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+      <div className={styles.gameCanvas}>
+        <div className={styles.card}>
+          {numbers.map((item, index) => {
+            if (item !== 0) {
+              return (
+                <div key={uuid()} className={styles.square}>
+                  <span
+                    style={
+                      closedNumbers.numbers.includes(numbers[index])
+                        ? { textDecoration: "line-through" }
+                        : {}
+                    }
+                  >
+                    {numbers[index]}
+                  </span>
+                </div>
+              );
+            } else return <div key={uuid()} className={styles.square}></div>;
+          })}
+          <button onClick={() => drawNumber()}>Draw a Number</button>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      </div>
     </>
-  )
+  );
 }
